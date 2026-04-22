bf0ddd41ffe3d342431abc6641932c1ea5b357a2
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-21 22:29:30 -0400

    Setup Tailwind CSS and configure Clerk environment variables

--- nextjs-supabase-app/components/Navbar.tsx
+++ nextjs-supabase-app/components/Navbar.tsx
@@ -8,9 +8,11 @@ function Navbar() {
       <div className="container mx-auto gap-2 py-3 px-4 sm:py-4 ">
         <div className="flex items-center space-x-2">
           {/* <Trello /> */}
-          <Kanban className="h-6 w-6 sm:h-8 sm:w-8" />
+          <Kanban className="h-6 w-6 sm:h-8 sm:w-8 text-blue-600" />
 
-          <span>Trello Clone</span>
+          <span className="text-xl sm:text-2xl text-gray-600 font-bold ">
+            Trello Clone
+          </span>
         </div>
       </div>
     </header>
