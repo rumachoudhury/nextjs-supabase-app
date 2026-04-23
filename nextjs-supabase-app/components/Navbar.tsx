e3562aceab400352673c4d69732c938f0244c5ba
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-22 23:07:18 -0400

    Fix Navbar user UI and dashboard link

--- nextjs-supabase-app/components/Navbar.tsx
+++ nextjs-supabase-app/components/Navbar.tsx
@@ -1,57 +1,10 @@
-// "use client";
-
-// import React from "react";
-// import { Kanban } from "lucide-react";
-// import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
-// import { Button } from "./ui/button";
-
-// function Navbar() {
-//   const { isSignIn, user } = useUser(); //useUser()  hook from Clerk to get the current user's authentication status and information.
-
-//   return (
-//     <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
-//       <div className="container mx-auto flex items-center justify-between py-3 px-4 sm:py-4">
-//         {/* Left side */}
-//         <div className="flex items-center space-x-2">
-//           <Kanban className="h-6 w-6 sm:h-8 sm:w-8 text-blue-600" />
-//           <span className="text-xl sm:text-2xl text-gray-600 font-bold">
-//             Trello Clone
-//           </span>
-//         </div>
-
-//         {/* Right side */}
-//         <div className="flex items-center space-x-2 sm:space-x-4">
-//           {/*SignInButton and SignUpButton from Clerk*/}
-//           {/*Button from shadcn UI*/}
-//           {isSignIn ? (
-//             <div className="flex flex-col sm:flex-row items-end"></div>
-//           ) : (
-//             <div>
-//               <SignInButton>
-//                 <Button variant="outline" size="sm" className="text-sm">
-//                   Sign In
-//                 </Button>
-//               </SignInButton>
-
-//               <SignUpButton>
-//                 <Button>Sign Up</Button>
-//               </SignUpButton>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default Navbar;
-// -----------------------------------
 "use client";
 
 import React from "react";
-import { Kanban } from "lucide-react";
+import { ArrowRight, Kanban } from "lucide-react";
 import { SignInButton, SignUpButton, useUser, UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 function Navbar() {
   const { isSignedIn, user } = useUser();
@@ -70,21 +23,32 @@ function Navbar() {
         {/* Right */}
         <div className="flex items-center space-x-2 sm:space-x-4">
           {isSignedIn && user && (
-            <span>
-              Welcome, {user.firstName ?? user.emailAddresses[0].emailAddress}
-            </span>
+            <>
+              <span className="text-xs sm:text-sm text-gray-600 hidden sm:block">
+                Welcome, {user.firstName ?? user.emailAddresses[0].emailAddress}
+              </span>
+
+              <Button asChild>
+                <Link href="/dashboard" className="flex items-center">
+                  Go to Dashboard
+                  <ArrowRight className="ml-2 h-4 w-4" />
+                </Link>
+              </Button>
+            </>
           )}
 
           {!isSignedIn ? (
             <>
               <SignInButton mode="modal">
-                <Button variant="outline" size="sm">
+                <Button variant="outline" size="sm" className="cursor-pointer">
                   Sign In
                 </Button>
               </SignInButton>
 
               <SignUpButton mode="modal">
-                <Button size="sm">Sign Up</Button>
+                <Button size="sm" className="cursor-pointer">
+                  Sign Up
+                </Button>
               </SignUpButton>
             </>
           ) : (
