29f4c8c4bea8409e58ab4f7094e1fff3a6dd371b
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-23 19:11:44 -0400

    Completed some parts with dashboard page

--- nextjs-supabase-app/app/dashboard/page.tsx
+++ nextjs-supabase-app/app/dashboard/page.tsx
@@ -10,10 +10,15 @@ function DashboardPage() {
     <div className="min-h-screen bg-fuchsia-50">
       <Navbar />
       <main className="container mx-auto px-4 py-6 sm:py-8 text-2xl font-semibold text-gray-800 text-center">
-        <h1>
-          Welcome to your dashboard!,{" "}
-          {user?.firstName ?? user?.emailAddresses[0].emailAddress}! 👋
-        </h1>
+        <div className="mb-6 sm:mb-8">
+          <h1 className="text-2xl sm:text-3xl font-bold text-gray-900 mb-2">
+            Welcome to your dashboard!,{" "}
+            {user?.firstName ?? user?.emailAddresses[0].emailAddress}! 👋
+          </h1>
+          <p className="text-gray-600">
+            Here&apos;s what&apos;s happening with your boards today
+          </p>
+        </div>
       </main>
     </div>
   );
