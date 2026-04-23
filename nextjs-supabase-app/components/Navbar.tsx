412babc2c618281a1411afae83fd67c593b277cf
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-22 22:38:52 -0400

    Fix Clerk user display in Navbar

--- nextjs-supabase-app/components/Navbar.tsx
+++ nextjs-supabase-app/components/Navbar.tsx
@@ -54,13 +54,12 @@ import { SignInButton, SignUpButton, useUser, UserButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 
 function Navbar() {
-  //useUser()  hook from Clerk to get the current user's authentication status and information.
-  const { isSignedIn } = useUser();
+  const { isSignedIn, user } = useUser();
 
   return (
     <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
       <div className="container mx-auto flex items-center justify-between py-3 px-4 sm:py-4">
-        {/* Left side */}
+        {/* Left */}
         <div className="flex items-center space-x-2">
           <Kanban className="h-6 w-6 sm:h-8 sm:w-8 text-blue-600" />
           <span className="text-xl sm:text-2xl text-gray-600 font-bold">
@@ -68,10 +67,14 @@ function Navbar() {
           </span>
         </div>
 
-        {/* Right side */}
+        {/* Right */}
         <div className="flex items-center space-x-2 sm:space-x-4">
-          {/*SignInButton and SignUpButton from Clerk*/}
-          {/*Button from shadcn UI*/}
+          {isSignedIn && user && (
+            <span>
+              Welcome, {user.firstName ?? user.emailAddresses[0].emailAddress}
+            </span>
+          )}
+
           {!isSignedIn ? (
             <>
               <SignInButton mode="modal">
