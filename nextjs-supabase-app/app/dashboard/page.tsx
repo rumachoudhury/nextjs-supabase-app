e2ecad500cf6847eab411065d4853b5c09e001e5
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-23 18:51:28 -0400

    Create dashboard page and add navbar

--- nextjs-supabase-app/app/dashboard/page.tsx
+++ nextjs-supabase-app/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import Navbar from "@/components/Navbar";
 import React from "react";
 
 function DashboardPage() {
-  return <div className="min-h-screen bg-fuchsia-50">page</div>;
+  return (
+    <div className="min-h-screen bg-fuchsia-50">
+      <Navbar />
+      <main className="container mx-auto px-4 py-6 sm:py-8 text-2xl font-semibold text-gray-800 text-center">
+        <h1>Welcome to your dashboard!</h1>
+      </main>
+    </div>
+  );
 }
 
 export default DashboardPage;
