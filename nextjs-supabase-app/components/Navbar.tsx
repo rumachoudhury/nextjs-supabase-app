485cc49abdf5541fefd9a450f361f0f12a6dab2f
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-23 16:39:16 -0400

    Fix navbar

--- nextjs-supabase-app/components/Navbar.tsx
+++ nextjs-supabase-app/components/Navbar.tsx
@@ -13,6 +13,31 @@ function Navbar() {
 
   const isHomePage = pathname === "/";
   const isDashboardPage = pathname === "/dashboard";
+  const isBoardPage = pathname.startsWith("/boards/"); //It checks if the current URL starts with /board/
+
+  if (isHomePage || isDashboardPage || isBoardPage) {
+    return (
+      <div>
+        <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+          <div className="container mx-auto flex items-center justify-between py-3 px-4 sm:py-4">
+            {/* Left */}
+            <div className="flex items-center space-x-2">
+              <Kanban className="h-6 w-6 sm:h-8 sm:w-8 text-blue-600" />
+              <span className="text-xl sm:text-2xl text-gray-600 font-bold">
+                Trello Clone
+              </span>
+            </div>
+
+            {/* Right */}
+            <div className="flex items-center space-x-2 sm:space-x-4">
+              <UserButton />
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
       <div className="container mx-auto flex items-center justify-between py-3 px-4 sm:py-4">
