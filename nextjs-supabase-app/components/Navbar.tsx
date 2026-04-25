acd249ab99ac22fb68a86de6d8ec0cef96f626d1
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-04-24 23:41:45 -0400

    Updated the Navbar

--- nextjs-supabase-app/components/Navbar.tsx
+++ nextjs-supabase-app/components/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar({
       <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
         <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <Kanban className="h-6 w-6 sm:h-8 sm:w-8 text-blue-600" />
+            <Kanban className="h-5 w-5 sm:h-8 sm:w-8 text-blue-600" />
             <span className="text-xl sm:text-2xl font-bold text-gray-900">
               TaskFlow
             </span>
@@ -125,18 +125,6 @@ export default function Navbar({
           <span className="text-xl sm:text-2xl font-bold text-gray-900">
             TaskFlow
           </span>
-
-          {/* <Link href="/" className="flex items-center space-x-2">
-            <Image
-              src="/images/TaskFlow.png"
-              alt="TaskFlow Logo"
-              width={52}
-              height={52}
-            />
-            <span className="text-xl sm:text-2xl font-bold text-gray-900">
-              TaskFlow
-            </span>
-          </Link> */}
         </div>
 
         <div className="flex items-center space-x-2 sm:space-x-4">
@@ -147,7 +135,7 @@ export default function Navbar({
               </span>
               <Link href="/dashboard">
                 <Button size="sm" className="text-xs sm:text-sm">
-                  Go to Dashboard <ArrowRight />
+                  Go to Dashboard <ArrowRight className="cursor-pointer" />
                 </Button>
               </Link>
             </div>
