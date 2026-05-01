9bc2d72347cde0700929c6a1f1d00a3d6fd24f47
Ruma Choudhury <rumachoudhury7862@gmail.com>
2026-05-01 12:40:15 -0400

    Created newBoard within useBoard.ts file

--- nextjs-supabase-app/lib/hooks/useBoards.ts
+++ nextjs-supabase-app/lib/hooks/useBoards.ts
@@ -1,8 +1,27 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { boardDataService } from "../supabase/services";
+
 export function useBoards() {
+
+  const user = useUser()
   async function createBoard(board: {
     title: string;
     description?: string;
     color: string;
-  }) {}
+  }) {
+
+    
+
+try {
+
+  const newBoard = await boardDataService.createBoardWithDefaultColumn...boardData,
+  userId: user?.id
+
+}
+
+
+  }
   return { createBoard };
 }
